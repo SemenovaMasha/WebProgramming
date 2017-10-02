ab5b42410557d0adf164f3f19dad2e1f2aac90c4
SemenovaMasha <semenovamasha98@mail.ru>
2017-10-02 09:16:13 +0400

    Initial commit

---
+++ WebLabs/test.php
@@ -0,0 +1,15 @@
+
+
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link rel="stylesheet" href ="styles/main.css"></link>
+    </head>
+    <body> 
+        <?php 
+    echo htmlspecialchars($_POST['login']); ?>
+    </body>
+</html>
